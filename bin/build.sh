e6f06a19e47706b561c0e0191abe83bebd1587e8
l3dlp <web@l3dlp.com>
2023-04-11 09:41:32 +0200

    Aperiodic update

--- bin/build.sh
+++ bin/build.sh
@@ -1,20 +1,18 @@
 #!/usr/bin/bash
 GOODPATH="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
 cd $GOODPATH
 cd ../
 
 # Screenshot
-cd $GOODPATH
-cd ../
 cat cmd/main.go | snippit --syntax go --out README.png
-boum .
 
 # Build
-cd $GOODPATH
-cd ../
 go mod tidy
-# reduced size: go build -ldflags "-s -w" -o build/pm cmd/main.go
 go build -ldflags "-s -w" -o bin/gophre cmd/main.go
+
+# Service
+boum .
 service gophre stop
 service gophre start
+gigit "Aperiodic update"
+
